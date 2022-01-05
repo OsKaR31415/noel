fce6e106b510b4656b90f292b15272cfec089cc3
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-01-05 19:18:39 +0100

    Update arbre_de_noel.py

--- arbre_de_noel.py
+++ arbre_de_noel.py
@@ -3,18 +3,28 @@ from math import cos, sin, pi
 import random
 from random import random
 from shutil import get_terminal_size
-from sys import maxsize
+from sys import maxsize, stdout
 
 def noise(point):
     return abs(hash(str(point)) / maxsize)
 
-H = int(10*get_terminal_size().lines/11 - 30/11)
-# H = 8X/9 - 8*3/9
+# the maths here are not obvious, because he basis of the tree is also
+# responsive... but it is not really important since 3 works up tu huge heights
+# (where the tree does not look like anything at all)
+H = int(10*get_terminal_size().lines/11 - 3)
 
+# create a colorizer (helps to color text in the terminal)
 color = Color()
-seed = 14
-border = 5
-result = color('yellow', "☆").center(H+1+2*border+7) + "\n"
+
+
+# size of the border (to display more snow)
+border = (get_terminal_size().columns - H)//2
+
+# initialize the text to print with new lines (so previouss text gets )
+result = "\n"*3
+
+# first put the star at the top of the tree !
+result += color('yellow', "☆").center(H+1+2*border+7) + "\n"
 
 for y in range(1, H):
     for x in range(-H//2 - border, H//2 + border):
@@ -61,8 +71,10 @@ result += color(28, ' '*(border) + "╱╲"*((1+H)//2))
 result += (H//8) * ('\n' + color(94, ("|"*(H//5)).center(H+2*border)))
 
 
+
 print(result)
 
 
+# very golfed and simplifed version :
 # print((lambda H:'^'.center(H+2)+'\n'+'\n'.join([''.join(['\\'if x==y//2 else('/' if x==-y//2 else(' 'if(x>y//2)or(x<-y//2)else'           *O~'[__import__('random').randint(0,13)]))for x in range(-H//2,H//2)])for y in range(2,H)]+['^'*H]+['| |'.center(H+2)]*(1+H//10)+['']))(int(input(">"))))
 
